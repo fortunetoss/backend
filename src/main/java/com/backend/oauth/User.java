16b2985ee406402e63c465d450d92f07640afbfc
cloud <goorm94@naver.com>
2024-12-16 21:59:19 +0900

    Feat: Social Login access token
    -Add Social Login access token issuing process
    -Add Userinfo save process
    -Add Security configuration
    -Add jwtFilter process

---
+++ src/main/java/com/backend/oauth/User.java
@@ -0,0 +1,24 @@
+package com.backend.oauth;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String username;
+
+    private String name;
+
+    private String email;
+
+    private String role;
+}
